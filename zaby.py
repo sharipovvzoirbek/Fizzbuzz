2dd0cb054306fbd340557540217b67cdc70e6d56
sharipovvzoirbek <zabyjorda@gmail.com>
2023-10-26 13:45:24 +0500

    FizzBuzz

--- zaby.py
+++ zaby.py
@@ -15,3 +15,20 @@ elif False:
 
 else:
     print("Fizz 3")
+    baha = 55
+zaby = 5
+a = baha%zaby
+d = a == 0
+
+
+
+
+if d:
+    print("buzz")
+
+elif False:
+
+    print("buzz")
+
+else:
+    print("buzz")
